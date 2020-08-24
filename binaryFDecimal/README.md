c6a04c9a0299ceca493ce251d05b04f42b17ad77
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2020-08-24 17:24:16 -0300

    Projeto conversor de números binários em decimais

---
+++ binaryFDecimal/README.md
@@ -0,0 +1,7 @@
+CONVERSOR DE NÚMERO BINÁRIO EM DECIMAL
+
+Aplicação retirada das ideias do github.com/florinpop17/app-ideas.
+
+O intuito é aprender cada vez mais sobre o mundo da programação e suas tecnologias.
+
+Espero que gostem deste código e possam usá-lo.
---
+++ binaryFDecimal/main.js
@@ -0,0 +1,12 @@
+convert = () => {
+    const binary = document.getElementById('bin').value;
+
+    if(binary === '') return alert('Por favor! Preencha o campo com um número binário válido');
+    binary.split('').map((char)=> {
+        if(char != '0' && char != '1') return alert('Atenção! Os números binários válidos são 0 ou 1');
+    });
+    const decimal = parseInt(binary, 2);
+    document.getElementById('res').value = decimal;
+    return true;
+}
+
